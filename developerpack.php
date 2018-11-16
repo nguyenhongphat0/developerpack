00a74378d1fda82dc302b70fe35a7b08d236a640
nguyenhongphat0 <hongphat.js@gmail.com>
2018-11-16 22:32:44 +0700

    init plugin with logo

---
+++ developerpack.php
@@ -0,0 +1,155 @@
+<?php
+/*
+*  @author nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
+*  @license https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0
+*/
+
+if (!defined('_PS_VERSION_')) {
+	exit;
+}
+
+class DeveloperPack extends Module
+{
+	public function __construct()
+	{
+		$this->name = 'developerpack';
+		$this->tab = 'administration';
+		$this->version = '1.0.0';
+		$this->author = 'nguyenhongphat0';
+		$this->need_instance = 0;
+		$this->ps_versions_compliancy = [
+			'min' => '1.6',
+			'max' => _PS_VERSION_
+		];
+		$this->bootstrap = true;
+
+		parent::__construct();
+
+		$this->displayName = $this->l('Developer pack');
+		$this->description = $this->l('This modules contain everything a prestashop developer need.');
+
+		$this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+
+		if (!Configuration::get('version')) {
+			$this->warning = $this->l('No version provided');
+		}
+	}
+
+    public function getContent()
+    {
+        $output = null;
+        if (Tools::isSubmit('submit'.$this->name)) {
+            $myModuleName = strval(Tools::getValue('MYMODULE_NAME'));
+
+            if (
+                !$myModuleName ||
+                empty($myModuleName) ||
+                !Validate::isGenericName($myModuleName)
+            ) {
+                $output .= $this->displayError($this->l('Invalid Configuration value'));
+            } else {
+                Configuration::updateValue('MYMODULE_NAME', $myModuleName);
+                $output .= $this->displayConfirmation($this->l('Settings updated'));
+            }
+        }
+        return $output.$this->displayInformation().$this->displayForm();
+    }
+
+    private function parsePhpinfo() {
+        ob_start(); phpinfo(INFO_MODULES); $s = ob_get_contents(); ob_end_clean();
+        $s = strip_tags($s, '<h2><th><td>');
+        $s = preg_replace('/<th[^>]*>([^<]+)<\/th>/', '<info>\1</info>', $s);
+        $s = preg_replace('/<td[^>]*>([^<]+)<\/td>/', '<info>\1</info>', $s);
+        $t = preg_split('/(<h2[^>]*>[^<]+<\/h2>)/', $s, -1, PREG_SPLIT_DELIM_CAPTURE);
+        $r = array(); $count = count($t);
+        $p1 = '<info>([^<]+)<\/info>';
+        $p2 = '/'.$p1.'\s*'.$p1.'\s*'.$p1.'/';
+        $p3 = '/'.$p1.'\s*'.$p1.'/';
+        for ($i = 1; $i < $count; $i++) {
+            if (preg_match('/<h2[^>]*>([^<]+)<\/h2>/', $t[$i], $matchs)) {
+                $name = trim($matchs[1]);
+                $vals = explode("\n", $t[$i + 1]);
+                foreach ($vals AS $val) {
+                    if (preg_match($p2, $val, $matchs)) { // 3cols
+                        $r[$name][trim($matchs[1])] = array(trim($matchs[2]), trim($matchs[3]));
+                    } elseif (preg_match($p3, $val, $matchs)) { // 2cols
+                        $r[$name][trim($matchs[1])] = trim($matchs[2]);
+                    }
+                }
+            }
+        }
+        return $r;
+    }
+
+    public function displayInformation() {
+        $phpinfo = json_encode($this->parsePhpinfo());
+        return '
+        <div class="panel">
+            <div class="panel-heading">Information</div>
+            <div>Current Prestashop version: '._PS_VERSION_.'</div>
+            <div>PHP Info: <pre style="height: 200px; overflow: auto" id="phpinfo"></pre></div>
+            <script>phpinfo = '.$phpinfo.';document.getElementById("phpinfo").innerText = JSON.stringify(phpinfo, null, 4);;</script>
+        </div>
+        ';
+    }
+
+    public function displayForm()
+    {
+        // Get default language
+        $defaultLang = (int)Configuration::get('PS_LANG_DEFAULT');
+
+        // Init Fields form array
+        $fieldsForm[0]['form'] = [
+            'legend' => [
+                'title' => $this->l('Settings'),
+            ],
+            'input' => [
+                [
+                    'type' => 'text',
+                    'label' => $this->l('Configuration value'),
+                    'name' => 'MYMODULE_NAME',
+                    'size' => 20,
+                    'required' => true
+                ]
+            ],
+            'submit' => [
+                'title' => $this->l('Save'),
+                'class' => 'btn btn-default pull-right'
+            ]
+        ];
+
+        $helper = new HelperForm();
+
+        // Module, token and currentIndex
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex.'&configure='.$this->name;
+
+        // Language
+        $helper->default_form_language = $defaultLang;
+        $helper->allow_employee_form_lang = $defaultLang;
+
+        // Title and toolbar
+        $helper->title = $this->displayName;
+        $helper->show_toolbar = true;        // false -> remove toolbar
+        $helper->toolbar_scroll = true;      // yes - > Toolbar is always visible on the top of the screen.
+        $helper->submit_action = 'submit'.$this->name;
+        $helper->toolbar_btn = [
+            'save' => [
+                'desc' => $this->l('Save'),
+                'href' => AdminController::$currentIndex.'&configure='.$this->name.'&save'.$this->name.
+                '&token='.Tools::getAdminTokenLite('AdminModules'),
+            ],
+            'back' => [
+                'href' => AdminController::$currentIndex.'&token='.Tools::getAdminTokenLite('AdminModules'),
+                'desc' => $this->l('Back to list')
+            ]
+        ];
+
+        // Load current value
+        $helper->fields_value['MYMODULE_NAME'] = Configuration::get('MYMODULE_NAME');
+
+        return $helper->generateForm($fieldsForm);
+    }
+}
