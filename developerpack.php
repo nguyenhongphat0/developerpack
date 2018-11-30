1b6f9d80ab05916629129204a4f0afa9ee5e7469
phatnh6 <phatnh6@fsoft.com.vn>
2018-11-30 14:42:23 +0700

    add product key

--- developerpack.php
+++ developerpack.php
@@ -1,11 +1,11 @@
 <?php
 /*
-*  @author nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
-*  @license https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0
-*/
+ *  @author nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
+ *  @license https://www.gnu.org/licenses/gpl-3.0.html GPL-3.0
+ */
 
 if (!defined('_PS_VERSION_')) {
-	exit;
+    exit;
 }
 
 /**
@@ -13,30 +13,30 @@ if (!defined('_PS_VERSION_')) {
  */
 class DeveloperPack extends Module
 {
-	public function __construct()
-	{
-		$this->name = 'developerpack';
-		$this->tab = 'administration';
-		$this->version = '1.0.0';
-		$this->author = 'nguyenhongphat0';
-		$this->need_instance = 0;
-		$this->ps_versions_compliancy = array(
-			'min' => '1.6',
-			'max' => _PS_VERSION_
-		);
-		$this->bootstrap = true;
+    public function __construct()
+    {
+        $this->name = 'developerpack';
+        $this->tab = 'administration';
+        $this->version = '1.0.0';
+        $this->author = 'nguyenhongphat0';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = array(
+            'min' => '1.6',
+            'max' => _PS_VERSION_
+        );
+        $this->bootstrap = true;
+        $this->module_key = '26d140b284bb3ecd72f3f0f09131ab45';
+        parent::__construct();
 
-		parent::__construct();
+        $this->displayName = $this->l('Developer pack');
+        $this->description = $this->l('This modules contain everything a prestashop developer need.');
 
-		$this->displayName = $this->l('Developer pack');
-		$this->description = $this->l('This modules contain everything a prestashop developer need.');
+        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
 
-		$this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
-
-		if (!Configuration::get('foo')) {
-			$this->warning = $this->l('No foo provided');
-		}
-	}
+        if (!Configuration::get('foo')) {
+            $this->warning = $this->l('No foo provided');
+        }
+    }
 
     public function getContent()
     {
@@ -58,12 +58,12 @@ class DeveloperPack extends Module
         return $output.$this->displayConfiguration();
     }
 
-	public function displayConfiguration() {
-		$this->context->smarty->assign(array(
-			'foo' => Configuration::get('foo'),
-			'root' => __PS_BASE_URI__,
-			'version' => _PS_VERSION_
-		));
-		return $this->display(__FILE__, 'views/templates/admin/index.tpl');
-	}
+    public function displayConfiguration() {
+        $this->context->smarty->assign(array(
+            'foo' => Configuration::get('foo'),
+            'root' => __PS_BASE_URI__,
+            'version' => _PS_VERSION_
+        ));
+        return $this->display(__FILE__, 'views/templates/admin/index.tpl');
+    }
 }
